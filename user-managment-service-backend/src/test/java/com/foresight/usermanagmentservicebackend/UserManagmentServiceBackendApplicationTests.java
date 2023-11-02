0d5e8204f6f054c096452282b116311b81d523bf
Abdelrahman Essam <abdelrahman.essaam@gmail.com>
2023-11-02 14:05:53 +0200

    creating empty spring boot project with dependencies

---
+++ user-managment-service-backend/src/test/java/com/foresight/usermanagmentservicebackend/UserManagmentServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foresight.usermanagmentservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagmentServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
