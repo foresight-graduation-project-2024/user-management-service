50a1496c1b78932d41e34b8e562b38afe2dc766f
Abdelrahman Essam <abdelrahman.essaam@gmail.com>
2023-11-02 14:18:18 +0200

    Recreating empty spring boot project with dependencies

---
+++ user-management-service-backend/src/main/java/com/foresight/usermanagementservicebackend/UserManagementServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foresight.usermanagementservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceBackendApplication.class, args);
+    }
+
+}
