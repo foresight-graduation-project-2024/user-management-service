ccaca01416dbce0eb4e69f2bd93ee1ba6cf7603b
S65001 <shihabmahmod65@gmail.com>
2023-12-22 04:35:31 +0200

    bug fixing

--- user-management-service-backend/src/main/java/com/foresight/usermanagementservicebackend/entity/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.foresight.usermanagementservicebackend.entity;
-
-public enum UserRole {
-
-    ADMIN, DRIVER, WORKER, SITE_ENGINEER, MONITORING_ENGINEER, TECHNICAL_MANAGER, BUSINESS_MANAGER;
-}
