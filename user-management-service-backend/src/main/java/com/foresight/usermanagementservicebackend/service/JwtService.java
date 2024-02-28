95b5b23d48135c169357e632d8e83ee057b2135e
S65001 <shihabmahmod65@gmail.com>
2024-02-28 22:21:24 +0200

    changing token time

--- user-management-service-backend/src/main/java/com/foresight/usermanagementservicebackend/service/JwtService.java
+++ user-management-service-backend/src/main/java/com/foresight/usermanagementservicebackend/service/JwtService.java
@@ -35,7 +35,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 *30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 *60*24*365))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
